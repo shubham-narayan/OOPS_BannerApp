01bf4bb2283b838d15c7e460de46d369285b268a
shubham-narayan <shubham.narayan.india@gmail.com>
2026-03-18 06:14:02 +0530

    UC6: Static Functions

--- src/BannerApp.java
+++ src/BannerApp.java
@@ -1,21 +1,51 @@
 public class BannerApp{
-     public static void main(String[] args) 
-    {
-        // Array declaration + initialization in single statement
-        String[] banner = {
-            String.join("   ", " *** ", " *** ", " *** ", " *** "),
-            String.join("   ", "*   *", "*   *", "*   *", "*    "),
-            String.join("   ", "*   *", "*   *", "*   *", "*    "),
-            String.join("   ", "*   *", "*   *", " *** ", " *** "),
-            String.join("   ", "*   *", "*   *", "*    ", "    *"),
-            String.join("   ", "*   *", "*   *", "*    ", "    *"),
-            String.join("   ", " *** ", " *** ", "*    ", " *** ")
+     public static String[] getO() {
+        return new String[] {
+            "  ****** ",
+            " **    **",
+            " **    **",
+            " **    **",
+            " **    **",
+            " **    **",
+            "  ****** "
         };
+    }
 
-        // Enhanced for-loop to print banner
-        for (String row : banner) 
-        {
-            System.out.println(row);
+    // Letter P (9 characters wide)
+    public static String[] getP() {
+        return new String[] {
+            " ******  ",
+            " **    **",
+            " **    **",
+            " ******  ",
+            " **       ",
+            " **       ",
+            " **       "
+        };
+    }
+
+    // Letter S (9 characters wide)
+    public static String[] getS() {
+        return new String[] {
+            "  ****** ",
+            " **      ",
+            " **      ",
+            "  *****  ",
+            "      ** ",
+            "      ** ",
+            " ******  "
+        };
+    }
+
+    public static void main(String[] args) {
+
+        String[] o1 = getO();
+        String[] o2 = getO();
+        String[] p  = getP();
+        String[] s  = getS();
+
+        for (int i = 0; i < o1.length; i++) {
+            System.out.println(o1[i] + "  " + o2[i] + "  " + p[i] + "  " + s[i]);
         }
     }
 }
