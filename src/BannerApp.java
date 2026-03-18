aba9c39e49ee30939512f380bf0d116ae12c9852
shubham-narayan <shubham.narayan.india@gmail.com>
2026-03-18 06:05:48 +0530

    UC4 improvements

--- src/BannerApp.java
+++ src/BannerApp.java
@@ -1,11 +1,16 @@
 public class BannerApp{
      public static void main(String[] args) {
-     System.out.println(String.join("", "   ***  ", "    ***  ", " ******   ", "    *****   "));
-     System.out.println(String.join("", " **  ** ", "  **  ** ", " **    ** ", "  **        "));
-     System.out.println(String.join("", "**    **", " **    **", " **     **", " **         "));
-     System.out.println(String.join("", "**    **", " **    **", " **    ** ", "   *****    "));
-     System.out.println(String.join("", "**    **", " **    **", " ******   ", "         ** "));
-     System.out.println(String.join("", " **  ** ", "  **  ** ", " **       ", "  **     ** "));
-     System.out.println(String.join("", "  ***   ", "   ***   ", " **       ", "    *****   "));
+        String[] banners = new String[7];
+        banners[0] = String.join("", "   ***  ", "    ***  ", " ******   ", "    *****   ");
+        banners[1] = String.join("", " **  ** ", "  **  ** ", " **    ** ", "  **        ");
+        banners[2] = String.join("", "**    **", " **    **", " **     **", " **         ");
+        banners[3] = String.join("", "**    **", " **    **", " **    ** ", "   *****    ");
+        banners[4] = String.join("", "**    **", " **    **", " ******   ", "         ** ");
+        banners[5] = String.join("", " **  ** ", "  **  ** ", " **       ", "  **     ** ");
+        banners[6] = String.join("", "  ***   ", "   ***   ", " **       ", "    *****   ");
+
+        for (int i = 0; i < banners.length; i++) {
+            System.out.println(banners[i]);
+        }
     }
 }
