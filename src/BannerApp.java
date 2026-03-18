ece893b84edc7cacc09fdd6f68f4bd00bb7b4337
shubham-narayan <shubham.narayan.india@gmail.com>
2026-03-18 05:59:37 +0530

    UC 3 improvements

--- src/BannerApp.java
+++ src/BannerApp.java
@@ -1,15 +1,11 @@
 public class BannerApp{
      public static void main(String[] args) {
-
-        // Each letter is 9 characters wide
-        // 7 lines total
-
-        System.out.println("    ***    " + "   ***     " + " ******  " + " ****** ");
-        System.out.println(" **     ** " + " **     ** " + " **    **" + " **     ");
-        System.out.println("**       **" + "**       **" + " **    **" + " **     ");
-        System.out.println("**       **" + "**       **" + " ******  " + " ****** ");
-        System.out.println("**       **" + "**       **" + " **      " + "     ** ");
-        System.out.println(" **     ** " + " **     ** " + " **      " + "     ** ");
-        System.out.println("    ***    " + "    ***    " + " **      " + " ****** ");
+     System.out.println(String.join("", "   ***  ", "    ***  ", " ******   ", "    *****   "));
+     System.out.println(String.join("", " **  ** ", "  **  ** ", " **    ** ", "  **        "));
+     System.out.println(String.join("", "**    **", " **    **", " **     **", " **         "));
+     System.out.println(String.join("", "**    **", " **    **", " **    ** ", "   *****    "));
+     System.out.println(String.join("", "**    **", " **    **", " ******   ", "         ** "));
+     System.out.println(String.join("", " **  ** ", "  **  ** ", " **       ", "  **     ** "));
+     System.out.println(String.join("", "  ***   ", "   ***   ", " **       ", "    *****   "));
     }
 }
