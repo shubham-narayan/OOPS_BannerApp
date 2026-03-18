42d8b9d9b5e761d2a02d4efa8cefcced98c4e3be
shubham-narayan <shubham.narayan.india@gmail.com>
2026-03-18 05:49:21 +0530

    UC2: Print Banner

--- src/BannerApp.java
+++ src/BannerApp.java
@@ -1,5 +1,15 @@
 public class BannerApp{
-    public static void main(String[] args) {
-     System.out.println("OOPS");   
+     public static void main(String[] args) {
+
+        // Each letter is 9 characters wide
+        // 7 lines total
+
+        System.out.println("    ***    " + "   ***     " + " ******  " + " ****** ");
+        System.out.println(" **     ** " + " **     ** " + " **    **" + " **     ");
+        System.out.println("**       **" + "**       **" + " **    **" + " **     ");
+        System.out.println("**       **" + "**       **" + " ******  " + " ****** ");
+        System.out.println("**       **" + "**       **" + " **      " + "     ** ");
+        System.out.println(" **     ** " + " **     ** " + " **      " + "     ** ");
+        System.out.println("    ***    " + "    ***    " + " **      " + " ****** ");
     }
 }
