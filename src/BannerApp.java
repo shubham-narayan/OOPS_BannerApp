3ec2e6ba71f8187e6586e0f3134fea4ff25c6721
shubham-narayan <shubham.narayan.india@gmail.com>
2026-03-18 06:10:38 +0530

    UC5: Array initialisation

--- src/BannerApp.java
+++ src/BannerApp.java
@@ -1,16 +1,21 @@
 public class BannerApp{
-     public static void main(String[] args) {
-        String[] banners = new String[7];
-        banners[0] = String.join("", "   ***  ", "    ***  ", " ******   ", "    *****   ");
-        banners[1] = String.join("", " **  ** ", "  **  ** ", " **    ** ", "  **        ");
-        banners[2] = String.join("", "**    **", " **    **", " **     **", " **         ");
-        banners[3] = String.join("", "**    **", " **    **", " **    ** ", "   *****    ");
-        banners[4] = String.join("", "**    **", " **    **", " ******   ", "         ** ");
-        banners[5] = String.join("", " **  ** ", "  **  ** ", " **       ", "  **     ** ");
-        banners[6] = String.join("", "  ***   ", "   ***   ", " **       ", "    *****   ");
+     public static void main(String[] args) 
+    {
+        // Array declaration + initialization in single statement
+        String[] banner = {
+            String.join("   ", " *** ", " *** ", " *** ", " *** "),
+            String.join("   ", "*   *", "*   *", "*   *", "*    "),
+            String.join("   ", "*   *", "*   *", "*   *", "*    "),
+            String.join("   ", "*   *", "*   *", " *** ", " *** "),
+            String.join("   ", "*   *", "*   *", "*    ", "    *"),
+            String.join("   ", "*   *", "*   *", "*    ", "    *"),
+            String.join("   ", " *** ", " *** ", "*    ", " *** ")
+        };
 
-        for (int i = 0; i < banners.length; i++) {
-            System.out.println(banners[i]);
+        // Enhanced for-loop to print banner
+        for (String row : banner) 
+        {
+            System.out.println(row);
         }
     }
 }
